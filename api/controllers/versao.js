b79e3b4dbb4ac0940c59cb0363a20c064435304a
marcindex <machollanda@gmail.com>
2026-08-02 19:22:28 +0000

    update api

--- api/controllers/versao.js
+++ api/controllers/versao.js
@@ -1,9 +1,10 @@
 module.exports = () => {
   const controller = {};
 
-  controller.get = (req, res) => {
-    const responseString = `Bia ${process.env.VERSAO_API || "2.0.0"}`;
+  controller.get = async (req, res) => {
+    const responseString = `Bia ${process.env.VERSAO_API || "4.3.0"}`;
     res.send(responseString);
   };
+
   return controller;
 };
