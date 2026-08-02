17af9447d3100faa386653cdf24404d59995bba0
marcindex <machollanda@gmail.com>
2026-08-02 15:53:44 +0000

    ajuste footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer>
-      <p>Copyright Oregon EAD 2023</p>
+      <p>Copyright Formação AWS 2026</p>
       <Link to="/about">Sobre o BIA</Link>
     </footer>
   );
